d871d4cf82bd024714c6d03822f0c964200547f9
Bob Wakefield <bob@MassStreet.net>
2017-10-27 22:53:19 -0500

    Live code test

--- Analysis/SparklyR/Sparklyr Example.R
+++ Analysis/SparklyR/Sparklyr Example.R
@@ -12,9 +12,70 @@ library(sparklyr)
 library(replyr) #This package has a dependency on wrapr
 sc = spark_connect(master="local") #This is NOT a Spark Context!
 
-titanic_train = spark_read_csv(sc, name="titanic_train", path="../../Data/titanic_train.csv", header = TRUE, delimiter = ",", quote = "\"", escape = "\\", charset = "UTF-8", null_value = NULL, repartition = 0, memory = TRUE, overwrite = TRUE)
+# Sparklyr Cheat Sheet Walk Through
 
-#replyr_summary(titanic_train)
+iris_tbl <- copy_to(sc, iris)
+
+set.seed(100)
+
+
+#Split Iris Data
+
+#Split the iris data into test/train sets
+#Register the training set
+#Create an R reference object for the training set
+import_iris = copy_to(sc,iris,"spark_iris", overwrite=TRUE)
+
+partition_iris <- sdf_partition(import_iris, training=0.5, testing=0.5)
+
+sdf_register(partition_iris, c("spark_iris_training", "spark_iris_test"))
+
+tidy_iris <- tbl(sc, "spark_iris_training") %>% select(Species, Petal_Length, Petal_Width)
+
+
+### Build and Train a Model in Spark
+
+
+model_iris <- tidy_iris %>% ml_decision_tree(response="Species", features=c("Petal_Length", "Petal_Width"))
+
+
+### Test the Model
+
+
+test_iris <- tbl(sc, "spark_iris_test")
+
+pred_iris <- sdf_predict(model_iris, test_iris) %>% collect
+
+
+### Visualize the Model Prediction
+
+
+library(ggplot2)
+
+pred_iris %>%inner_join(data.frame(prediction=0:2, lab=model_iris$model.parameters$labels)) %>%ggplot(aes(Petal_Length, Petal_Width, col=lab)) + geom_point()
+
+
+#Evaluate the Model Prediction
+
+#Create an evaluation response column in the data
+
+
+pred_iris2 <- pred_iris %>%
+mutate(predSpecies = ifelse(test = (prediction == 1), yes = "setosa", no = ifelse(test = (prediction == 0), yes = "virginica",
+                                                                                    no = "versicolor"))) %>%
+  mutate(correct = ifelse(test = (predSpecies == Species), yes = 1, no = 0)) %>%
+  collect
+
+
+#Visualize labeled data
+
+
+pred_iris2 %>% ggplot(aes(Petal_Length, Petal_Width, col=correct)) + geom_point()
+
+
+
+
+
 
 spark_disconnect(sc)
 
